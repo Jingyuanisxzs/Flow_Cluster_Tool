8c9f12ee7c2bbb0dd9807960eae8836eb488300a
TS Planning Engineer <ym5@ualberta.ca>
2018-07-26 16:19:59 -0600

    clean spaces

--- public/javascripts/selection.js
+++ public/javascripts/selection.js
@@ -5,7 +5,6 @@ $( document ).ready(function() {
       socket.emit('chat message',$('#scenario').val()+'_'+$('#year').val()+'_'+$('#version').val());
       return false;
     });
-
     socket.on('finish',function(msg){
       alert('Finish Decoding: '+msg);
     });
@@ -42,41 +41,31 @@ $( document ).ready(function() {
       $("#omxTable").append('<tr><th onclick="sortTable(0,omxTable)">OMX File</th></tr>');
 
       for(var i in msg){
-        omxList.push(msg[i])
+        omxList.push(msg[i]);
       }
       for(var i = 0;i<omxList.length;i++){
-  
           var splitedData = omxList[i].split('_');
           var omxFileName = omxList[i].split('.');
           if(splitedData.length>4){
               $("#omxTable").append('<tr class="clickableRow3"><td>'+omxFileName[0]+'</td></tr>');
-  
               validFlowList.push(omxList[i]);
-  
               var option = document.createElement("option");
               option.text = splitedData[2];
               option.value =splitedData[2];
-  
               var select = document.getElementById("scenario");
               select.appendChild(option);
               var optionYear = document.createElement("option");
-  
               optionYear.text = splitedData[3];
               optionYear.value =splitedData[3];
               var selectYear = document.getElementById("year");
               selectYear.appendChild(optionYear);
-  
-  
               var optionVersion = document.createElement("option");
-  
               optionVersion.text = splitedData[4].split('.')[0];
               optionVersion.value =splitedData[4].split('.')[0];
               var selectVersion = document.getElementById("version");
               selectVersion.appendChild(optionVersion);
-  
           }
       }
-  
       $(".clickableRow3").on("click",function(){
           var rowItem = $(this).children('td').map(function () {
               return this.innerHTML;
@@ -90,10 +79,7 @@ $( document ).ready(function() {
           year = splitRowItem[3];
           $('#queryYear').val(year);
           $('#newQueryYear').val(year);
-  
           $('#year').val(year);
-  
-  
           version = splitRowItem[4];
           $('#queryVersion').val(version);
           $('#newQueryVersion').val(version);
@@ -104,24 +90,16 @@ $( document ).ready(function() {
           scenario=document.getElementById('scenario').value;
           $('#queryScenario').val(scenario);
           $('#newQueryScenario').val(scenario);
-  
       });
       $('#year').on('click',function(){
           year = document.getElementById('year').value;
           $('#queryYear').val(year);
           $('#newQueryYear').val(year);
-  
-  
       });
       $('#version').on('click',function(){
           version = document.getElementById('version').value;
           $('#queryVersion').val(version);
           $('#newQueryVersion').val(version);
-  
       });
-    })
-  
-
-
-
+    });
 });
