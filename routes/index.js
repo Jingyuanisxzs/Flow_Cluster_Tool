a7d6d8b1263d201e174646c8133c941b576f81bd
Harley Gorrell <harley@panix.com>
2018-08-10 16:20:31 -0600

    fct-version-data: Display the git version and time of the last update.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 /*jshint esversion: 6 */
+var fs=require('fs');
 var router = require('express').Router();
+
 // The top of our dir.
 var FCT_DIR = process.env.FCT_DIR;
 console.log("FCT_DIR=",FCT_DIR);
+
+//
+function fct_read_version_data() {
+  var version_path=FCT_DIR+"/public/version.txt";
+  
+  if (fs.existsSync(version_path)) {
+    return fs.readFileSync(version_path)
+  }
+  return "DEV";
+}
+var FCT_VERSION_DATA=fct_read_version_data();
+
+console.log("FCT_VERSION_DATA="+FCT_VERSION_DATA);
+
+//////////
+
 function walkfolders(dir) {
     var fs = fs || require('fs'),
         files = fs.readdirSync(dir);
@@ -26,4 +44,10 @@ router.get('/flow_data',function(req,res,next){
     res.render('index', { title: 'Flow Cluster Analysis Tool'});
 });
 
+
+// 
+router.get('/version.txt',function(req,res,next) {
+  res.send(FCT_VERSION_DATA);
+});
+  
 module.exports = router;
